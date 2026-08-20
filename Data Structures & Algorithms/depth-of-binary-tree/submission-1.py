62fb41f2e6ba3544fdb5b9fe4248ad0e9aa151b1
Shamir Aryal <shamir.795421@ac.tu.edu.np>
2026-08-20 21:39:51 +0545

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #Fuck the coding....
+        if not root:
+            return 0
+        depthL = self.maxDepth(root.left)#this shit of lines count left nodes depth suppose we have to nodes left and right firstly it calculate max depth amongst all of child nodes at left sides
+        depthR = self.maxDepth(root.right)
+
+        return 1 + max(depthL,depthR)
+        
