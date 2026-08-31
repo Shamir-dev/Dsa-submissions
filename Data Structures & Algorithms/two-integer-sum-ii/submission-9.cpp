6ace00e4e29396d6df6b96180a55cbc6d2b5d9b5
Shamir Aryal <shamir.795421@ac.tu.edu.np>
2026-08-31 08:59:18 +0545

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res(100);
+        unordered_map<int, int> seen;
+        for( int i =0; i<numbers.size(); i++){
+            int required = target - numbers[i];
+            if (seen.count(required)) {
+                res = {seen[required]+1, i+1};
+                return res;
+
+            }
+            else{
+                seen[numbers[i]] = i;
+            }
+
+            // for (int j = i+1;j<numbers.size(); j++) {
+            //     if (numbers[i] + numbers[j] == target){
+            //         res = {i+1,j+1};
+            //         return res;
+            //     }
+            // }
+        }
+        return res;
+      
+    }
+};
